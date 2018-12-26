185f38a1399d994c2556ca997f6992cd4e8c0e84
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2018-12-26 22:27:20 +0500

    Wrapped helpers.php functions in conditions to make sure they are not already defined

--- src/helpers.php
+++ src/helpers.php
@@ -1,27 +1,34 @@
 <?php
 
-/**
- * Get instance class name without namespace
- *
- * @param $instance
- *
- * @return string
- */
-function className($instance)
-{
-    return (new \ReflectionClass($instance))->getShortName();
+if ( ! function_exists('className')) {
+
+    /**
+     * Get instance class name without namespace
+     *
+     * @param $instance
+     *
+     * @return string
+     */
+    function className($instance)
+    {
+        return (new \ReflectionClass($instance))->getShortName();
+    }
+
 }
 
-function str_to_words($input)
-{
-    $re     = '/(?#! splitCamelCase Rev:20140412)
+if ( ! function_exists('str_to_words')) {
+    function str_to_words($input)
+    {
+        $re     = '/(?#! splitCamelCase Rev:20140412)
     # Split camelCase "words". Two global alternatives. Either g1of2:
       (?<=[a-z])      # Position is after a lowercase,
       (?=[A-Z])       # and before an uppercase letter.
     | (?<=[A-Z])      # Or g2of2; Position is after uppercase,
       (?=[A-Z][a-z])  # and before upper-then-lower case.
     /x';
-    $result = preg_split($re, $input);
+        $result = preg_split($re, $input);
 
-    return implode(" ", $result);
+        return implode(" ", $result);
+    }
+
 }
