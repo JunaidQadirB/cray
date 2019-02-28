346541392193dfbed13108927a22b705024558a8
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2019-02-28 23:57:58 +0500

    v1.2.0

---
+++ src/Traits/RedirectsWithFlash.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MoonBear\LaravelCrudScaffold\Traits;
+
+
+trait RedirectsWithFlash
+{
+    public function success($message, $route, ...$routeArgs)
+    {
+        request()->session()->flash('alert-success', $message);
+        if ($route === 'back') {
+            return redirect()->back();
+
+        }
+
+        return redirect()->route($route, ...$routeArgs);
+
+    }
+
+    public function error($message, $route)
+    {
+        request()->session()->flash('alert-danger', $message);
+
+        return redirect()->route($route);
+
+    }
+
+    public function info($message, $route)
+    {
+        request()->session()->flash('alert-info', $message);
+
+        return redirect()->route($route);
+
+    }
+}
