b84efd0c943028a9f38589b673bce7d69e7d746c
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2021-01-18 02:50:03 +0500

    Testing out form generation functionality via migrations

--- src/Cray.php
+++ src/Cray.php
@@ -2,7 +2,42 @@
 
 namespace JunaidQadirB\Cray;
 
+use Illuminate\Support\Str;
+
 class Cray
 {
-    // Build your next great package.
+    public function generateFormFields($tableName): array
+    {
+        $fields = [];
+        $table = $this->getTable($tableName);
+
+        foreach ($table->getColumns() as $column) {
+            $name = $column->getName();
+            $label = Str::of($name)->replace('_', ' ')->title;
+
+            switch ($column->getType()->getName()) {
+                case 'string':
+                    $fields[] = [
+                        'component' => 'themes.default.input',
+                        'type' => 'text',
+                        'label' => $label,
+                        'name' => $name,
+                        'label_i18n' => __('labels.'.$name)
+                    ];
+                    break;
+            }
+        }
+
+        return $fields;
+    }
+
+    private function getTable($name)
+    {
+        $schema = \DB::getDoctrineSchemaManager();
+
+        return collect($schema->listTables())
+            ->filter(function ($table) use ($name) {
+                return $table->getName() === $name;
+            })->first();
+    }
 }
