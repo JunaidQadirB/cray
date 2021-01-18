8eb6a11c8855e42d853eab77349041f7d3c57921
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2021-01-18 21:56:09 +0500

    WIP

--- config/config.php
+++ config/config.php
@@ -1,4 +1,12 @@
 <?php
+
 return [
     "stubs_dir" => resource_path('stubs'),
+    "component_paths" => [
+        'input.text' => 'components.text',
+        'input.number' => 'components.number',
+        'input.checkbox' => 'components.checkbox',
+        'input.radio' => 'components.radio',
+        'input.file' => 'components.file',
+    ]
 ];
--- src/Cray.php
+++ src/Cray.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Str;
 
 class Cray
 {
-    public function generateFormFields($tableName): array
+    public static function fields($tableName, $model = null, $markup = false)
     {
         $fields = [];
-        $table = $this->getTable($tableName);
+        $table = static::getTable($tableName);
 
         foreach ($table->getColumns() as $column) {
             $name = $column->getName();
@@ -18,20 +18,36 @@ class Cray
             switch ($column->getType()->getName()) {
                 case 'string':
                     $fields[] = [
-                        'component' => 'themes.default.input',
+                        'component' => config('component_paths.input.text'),
                         'type' => 'text',
                         'label' => $label,
                         'name' => $name,
-                        'label_i18n' => __('labels.'.$name)
+                        'label_i18n' => __('labels.'.$name),
+                        'value' => $model->$name ?? null,
                     ];
+                    break;
+                case 'text':
+                    if ($name == 'photo') {
+                        $fields[] = [
+                            'component' => config('component_paths.input.file'),
+                            'type' => 'text',
+                            'label' => $label,
+                            'name' => $name,
+                            'label_i18n' => __('labels.'.$name),
+                            'value' => $model->$name ?? null,
+                        ];
+                    }
                     break;
             }
         }
 
+        if ($markup) {
+            return static::generateMarkup($fields);
+        }
         return $fields;
     }
 
-    private function getTable($name)
+    private static function getTable($name)
     {
         $schema = \DB::getDoctrineSchemaManager();
 
@@ -39,5 +55,24 @@ class Cray
             ->filter(function ($table) use ($name) {
                 return $table->getName() === $name;
             })->first();
+    }
+
+    public static function generateMarkup($fields)
+    {
+        $markup = '';
+
+        foreach ($fields as $field) {
+            $markup .= <<<ENDL
+<x-dynamic-component
+        component="{$field['component']}"
+        name="{$field['name']}"
+        label="{$field['label_i18n']}"
+        type="{$field['type']}"
+        value="{$field['value']}" />
+ENDL;
+            $markup .= "\n";
+        }
+
+        return $markup;
     }
 }
